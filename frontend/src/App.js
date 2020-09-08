e1aaf7b727c17968853c7a4c43e249343e542efb
Adam <adamsarach@gmail.com>
2020-09-08 18:05:50 +0200

    Main Page First Draft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
+import ReactDOM from "react-dom"
+
 import logo from './logo.svg';
 import './App.css';
+import Main_photo from './checkers_placeholder.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    showLoginPage() {
+        alert('Login Page!');
+    };
+
+    showRegisterPage() {
+        alert('RegisterPage!');
+    };
+
+    render() {
+        return (
+            <div id="outer-box">
+                <div id="main-box">
+                    <div>
+                        <h3 className="centered" id="title">Welcome to Checkers!</h3>
+                    </div>
+                    <div>
+                        {/*{Main_photo}*/}
+                        <img id="placeholder-photo" src={Main_photo} alt="Placeholder"/>
+                    </div>
+                    <div>
+                        <p className="close-to-bottom centered">Please <span className="span-link"
+                            onClick={this.showLoginPage}> sign in </span> or <span className="span-link"
+                            onClick={this.showRegisterPage}> register </span> to play a game.</p>
+                    </div>
+                </div>
+            </div>
+
+
+        )
+    }
 }
 
 export default App;
+
+
+class Mainphoto extends React.Component {
+    render() {
+        return
+    };
+}
